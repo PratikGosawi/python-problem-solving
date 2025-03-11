5a074704e14c5c8045c1e0bd2767d9d7179ca9aa
PratikGosawi <prateikgosawi7@gmail.com>
2025-03-11 20:31:27 +0530

    GFG - Solution to recursion problem Power Using Recursion

---
+++ 37. Power Using Recursion.py
@@ -0,0 +1,28 @@
+# User function Template for python3
+
+class Solution:
+    def RecursivePower(self, n, p):
+        '''
+        return value of n^p recursively;
+        '''
+        if p == 0:
+            return 1
+
+        return n * self.RecursivePower(n, p - 1)
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n, p = map(int, input().strip().split())
+        print(Solution().RecursivePower(n, p))
+        print("~")
+# } Driver Code Ends
