98bfe22d65321683b153316e6d2351f05e2da093
PratikGosawi <119072846+PratikGosawi@users.noreply.github.com>
2026-08-04 17:24:17 +0530

    Add solution for Power Of Four

--- LeetCode/Power Of Four/solution.py
+++ LeetCode/Power Of Four/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n <= 1 or n % 4 == 0:
+        if n == 1 or n % 4 == 0:
             return True
         else:
             return False
