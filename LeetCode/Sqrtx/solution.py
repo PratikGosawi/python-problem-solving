0c12329da23ffa0c76a49cd7643af925fccf5a03
PratikGosawi <119072846+PratikGosawi@users.noreply.github.com>
2026-08-04 13:05:39 +0530

    Add solution for Sqrtx

--- LeetCode/Sqrtx/solution.py
+++ LeetCode/Sqrtx/solution.py
@@ -5,7 +5,7 @@ class Solution:
 
         mid = x // 2
 
-        for i in range(1, mid+1):
+        for i in range(1, x):
             if i*i == x:
                 return i
             elif i*i > x:
