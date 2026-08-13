1b7f1e724f722dca683c74a492924743fabc51ef
PratikGosawi <119072846+PratikGosawi@users.noreply.github.com>
2026-08-13 14:11:41 +0530

    Add solution for Factorial Trailing Zeroes

--- LeetCode/Factorial Trailing Zeroes/solution.py
+++ LeetCode/Factorial Trailing Zeroes/solution.py
@@ -4,16 +4,19 @@ class Solution:
             return 0
 
         count = 0
+        # while n != 0:
+        #     if n % 5 == 0:
+        #         count += 1
+        #         num = n
+        #         while num % 25 == 0:
+        #             count += 1
+        #             num = num / 5
 
-        while n != 0:
-            if n % 5 == 0:
-                count += 1
-                num = n
-                while num % 25 == 0:
-                    count += 1
-                    num = num / 5
+        #     n -= 1
 
-            n -= 1
+        while n >= 5:
+            n = n // 5
+            count += n
+
         return count
 
-count 
