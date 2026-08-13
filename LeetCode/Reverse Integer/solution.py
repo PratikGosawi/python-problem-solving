beabf3272384f36ac3f83918451122f73e27a7f8
PratikGosawi <119072846+PratikGosawi@users.noreply.github.com>
2026-08-13 10:36:46 +0530

    Add solution for Reverse Integer

--- LeetCode/Reverse Integer/solution.py
+++ LeetCode/Reverse Integer/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        print((x > 2**31 -1))
         num = x
         x = abs(x)
         result = 0
         while x != 0:
             mod_num = x % 10
             result = (result * 10) + mod_num
+            if result > 2**31:
+                return 0
             x = int(x / 10)
-            if (x < -2**31) or (x > 2**31 -1):
-                x = 0
+            
         
         if num < 0:
             return 0 - result
